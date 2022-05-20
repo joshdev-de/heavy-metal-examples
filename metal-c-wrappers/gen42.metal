c7f696525b578af9f2534588ec615514a3498730
joshdev-de <joshua.steinmann@ru.nl>
2022-05-20 12:31:58 +0200

    add example that uses GPU notably

--- metal-c-wrappers/gen42.metal
+++ metal-c-wrappers/gen42.metal
@@ -5,3 +5,8 @@ kernel void gen42(device float* a, uint index [[thread_position_in_grid]])
 {
     a[index] = 42.0;
 }
+
+kernel void addOne(device float* a, uint index [[thread_position_in_grid]])
+{
+    a[index] = a[index] + 1;
+}
--- metal-c-wrappers/use-wrapper.c
+++ metal-c-wrappers/use-wrapper.c
@@ -11,30 +11,54 @@ int main()
     void *device = getDefaultDevice();
     library_ptr library = loadDefaultLibrary(device);
     cmd_queue_ptr cmd_queue = getCommandQueue(device);
-    cmd_buffer_ptr cmd_buffer = getCommandBuffer(cmd_queue);
 
-    pso_ptr pso = psoWithFunction(device, library, "gen42");
+    pso_ptr pso_gen42 = psoWithFunction(device, library, "gen42");
+    pso_ptr pso_addOne = psoWithFunction(device, library, "addOne");
 
-    comp_cmd_enc_ptr cmd_enc = getComputeCmdEncoder(cmd_buffer);
-    setComputePipelineState(cmd_enc, pso);
+    cmd_buffer_ptr cmd_buffer;
 
     device_buffer_ptr device_buffer = newDeviceBuffer(device, sizeof(float) * ARRAY_SIZE);
 
+    // initialize arry with 42.0
+
+    cmd_buffer = getCommandBuffer(cmd_queue);
+    comp_cmd_enc_ptr cmd_enc = getComputeCmdEncoder(cmd_buffer);
+    
+    setComputePipelineState(cmd_enc, pso_gen42);
     setBuffer(cmd_enc, device_buffer, 0, 0);
 
     mtl_size gridSize = {ARRAY_SIZE, 1, 1};
-    mtl_size threadGroupSize = {maxTotalThreadsPerThreadgroup(pso), 1, 1};
-
+    mtl_size threadGroupSize = {maxTotalThreadsPerThreadgroup(pso_addOne), 1, 1};
     dispatchThreads(cmd_enc, gridSize, threadGroupSize);
     endEncoding(cmd_enc);
 
     commit(cmd_buffer);
 
+
+    // add 1 to each element a couple of times
+
+    for(int i = 0; i < 1000000; i++) {
+        cmd_buffer = getCommandBuffer(cmd_queue);
+        comp_cmd_enc_ptr cmd_enc = getComputeCmdEncoder(cmd_buffer);
+        setComputePipelineState(cmd_enc, pso_addOne);
+
+        setBuffer(cmd_enc, device_buffer, 0, 0);
+
+        mtl_size gridSize = {ARRAY_SIZE, 1, 1};
+        mtl_size threadGroupSize = {maxTotalThreadsPerThreadgroup(pso_addOne), 1, 1};
+
+        dispatchThreads(cmd_enc, gridSize, threadGroupSize);
+        endEncoding(cmd_enc);
+
+        commit(cmd_buffer);
+    }
+    
+
     waitUntilCompleted(cmd_buffer);
 
     float *content = bufferContents(device_buffer);
 
-    printf("%f", content[42]);
+    printf("%f", content[12]);
 
     return 0;
 }
